a8fb60b2fd34e281e9f7790ae3e7192bc8591c34
Laurent P. René de Cotret <laurent.renedecotret@bitnomial.com>
2026-06-16 20:41:06 -0400

    Add a check in CI for Ormolu formatting

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
-import Test.Tasty (testGroup, defaultMain)
 import Test.Network.Consensus.Raft (tests)
+import Test.Tasty (defaultMain, testGroup)
 
 main :: IO ()
 main = defaultMain (testGroup "hs-raft" [tests])
